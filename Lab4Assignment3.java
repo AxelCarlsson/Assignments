69fc78b24fedaf1ebe719a7588af791801ab5dba
AxelCarlsson <axel99.carlsson@gmail.com>
2025-09-10 12:36:18 +0200

    Lab 4 uppgift 3

---
+++ Lab4Assignment3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Lab4Assignment3 {
+    public static void main(String[] args) {
+        /*
+         * Användaren skriver in ett tal
+         * Skriv ut multiplikationstabellen 
+         * för talet
+         * 
+         * FÖR input * i upp till i = 10
+         */
+        System.out.println("Vilken multiplikationstabbel vill du använda?");
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+
+        for (int i = 1; i < 11; i++){
+            System.out.println(i * input);
+        }
+    }
+}
