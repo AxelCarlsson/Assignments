f522835ccc145dece86d98241faf37aea9e535ef
AxelCarlsson <axel99.carlsson@gmail.com>
2025-09-11 21:37:38 +0200

    Lab 4 assignment 11

---
+++ Lab4Assignment11.java
@@ -0,0 +1,20 @@
+public class Lab4Assignment11 {
+    public static void main(String[] args) {
+        /*
+         * Användare trycker in kort
+         * Skriver in PIN
+         * 
+         * OM PIN ogiltig
+         *  mata ut kortet
+         *  visa felmeddelande
+         * ANNARS
+         *  användare anger uttagsbelopp
+         *  OM saldo < uttagsbelopp
+         *   mata ut kortet
+         *   visa felmeddelande
+         *  ANNARS
+         *   mata ut utagsbeloppet
+         *   mata ut kortet
+         */
+    }
+}
