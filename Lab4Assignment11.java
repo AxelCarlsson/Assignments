e16e8064f0ef86a9e6382a16fb387ccf14662839
AxelCarlsson <axel99.carlsson@gmail.com>
2025-09-15 11:41:05 +0200

    Lab 4 assignment 11 added stuff

--- Lab4Assignment11.java
+++ Lab4Assignment11.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Lab4Assignment11 {
     public static void main(String[] args) {
         /*
@@ -16,5 +18,29 @@ public class Lab4Assignment11 {
          *   mata ut utagsbeloppet
          *   mata ut kortet
          */
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insert PIN");
+
+        int pin = 1234;
+        int saldo = 5000;
+
+        int input = sc.nextInt();
+
+         // TODO regex match 4 numbers maybe?
+
+        if (input != pin){
+            System.out.println("Wrong PIN. Spitting out card");
+        } else {
+            System.out.println("Choose withdrawal amount");
+            int withdrawal = sc.nextInt();
+            if (withdrawal > saldo){
+                System.out.println("Insufficient funds. Spitting out card");
+            } else {
+                System.out.println("Heres your " + withdrawal + "kr");
+            }
+        }
+
     }
 }
